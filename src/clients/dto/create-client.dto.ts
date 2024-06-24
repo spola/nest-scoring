ff4eed54028c4bda23dd5cbb3cae015dacc8a83c
Sergio Pola <sergio.pola@keylogistics.cl>
2024-06-24 13:28:40 -0400

    Dto para creación de client

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,1 +1,19 @@
-export class CreateClientDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateClientDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  rut: string;
+
+  @IsNotEmpty()
+  @IsNumber({ allowNaN: false })
+  salary: number;
+
+  @IsNotEmpty()
+  @IsNumber({ allowNaN: false })
+  savings: number;
+}
---
+++ src/clients/dto/dto.spec.ts
@@ -0,0 +1,115 @@
+import { ValidationError, validate } from 'class-validator';
+import { CreateClientDto } from './create-client.dto';
+import { error } from 'console';
+import { plainToInstance } from 'class-transformer';
+import { ClientEntity } from '../entities/client.entity';
+
+//@ValidateNested()
+
+describe('Clients DTO', () => {
+  let dto: CreateClientDto;
+  let dtoPlain;
+
+  beforeEach(async () => {
+    dtoPlain = {
+      name: "Juan Pérez O'Higgins Ñuble",
+      rut: '22.222.222-2',
+      salary: 100000,
+      savings: 5000000,
+    };
+    dto = plainToInstance(CreateClientDto, dtoPlain);
+  });
+
+  describe('should CreatePruebaDto', () => {
+    it('be valid', async () => {
+      let errores: ValidationError[];
+
+      errores = await validate(dto);
+      expect(errores).toHaveLength(0);
+    });
+    it('be invalid salary', async () => {
+      let errores: ValidationError[];
+
+      dto.salary = null;
+      errores = await validate(dto);
+      expect(errores).toHaveLength(1);
+
+      let invalid = plainToInstance(CreateClientDto, {
+        ...dtoPlain,
+        salary: '',
+      });
+      errores = await validate(invalid);
+      expect(errores).toHaveLength(1);
+
+      invalid = plainToInstance(CreateClientDto, {
+        ...dtoPlain,
+        salary: 'no_numero',
+      });
+      errores = await validate(invalid);
+      expect(errores).toHaveLength(1);
+    });
+    it('be invalid savings', async () => {
+      let errores: ValidationError[];
+
+      dto.savings = null;
+      errores = await validate(dto);
+      expect(errores).toHaveLength(1);
+
+      let invalid = plainToInstance(CreateClientDto, {
+        ...dtoPlain,
+        savings: '',
+      });
+      errores = await validate(invalid);
+      expect(errores).toHaveLength(1);
+
+      invalid = plainToInstance(CreateClientDto, {
+        ...dtoPlain,
+        savings: 'no_numero',
+      });
+      errores = await validate(invalid);
+      expect(errores).toHaveLength(1);
+    });
+    it('be invalid name', async () => {
+      let errores: ValidationError[];
+
+      dto.name = null;
+      errores = await validate(dto);
+      expect(errores).toHaveLength(1);
+
+      let invalid = plainToInstance(CreateClientDto, {
+        ...dtoPlain,
+        name: '',
+      });
+      errores = await validate(invalid);
+      expect(errores).toHaveLength(1);
+    });
+    it('be invalid rut', async () => {
+        let errores: ValidationError[];
+  
+        dto.rut = null;
+        errores = await validate(dto);
+        expect(errores).toHaveLength(1);
+  
+        let invalid = plainToInstance(CreateClientDto, {
+          ...dtoPlain,
+          rut: '',
+        });
+        errores = await validate(invalid);
+        expect(errores).toHaveLength(1);
+      });
+    // it("be invalid cantidad", async () => {
+
+    //     let dto = new CreatePruebaDto();
+    //     let errores: ValidationError[];
+
+    //     dto.detalle = "Un mensaje";
+    //     errores = await validate(dto);
+
+    //     expect(errores).toHaveLength(1);
+
+    //     expect(errores[0].constraints).toHaveProperty("isNotEmpty")
+    //     expect(errores[0].constraints).toHaveProperty("isNumber")
+
+    // });
+  });
+});
