ab50a3da3aee5e2c784aac6269fb05de17afe330
Sergio Pola <sergio.pola@keylogistics.cl>
2024-06-25 12:34:27 -0400

    Configuración del módulo

---
+++ src/scoring/dto/client-scoring-vector.dto.ts
@@ -0,0 +1,7 @@
+export class ClientScoringVectorDTO {
+  interest: number;
+  purshaseCapacity: number;
+  paymentCapacity: number;
+  monthOfDebts: number;
+  realSavings: number;
+}
--- src/scoring/scoring.module.ts
+++ src/scoring/scoring.module.ts
@@ -5,9 +5,10 @@ import { MessageEntity } from '../clients/entities/message.entity';
 import { DebtEntity } from '../clients/entities/debt.entity';
 
 import { ScoringService } from './scoring.service';
+import { ClientToScoreView } from './entities/client-to-score-view.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ClientEntity, MessageEntity, DebtEntity/*, ClientToDoFollowUpView*/])],
+  imports: [TypeOrmModule.forFeature([ClientEntity, MessageEntity, DebtEntity, ClientToScoreView])],
   providers: [ScoringService]
 })
 export class ScoringModule {}
