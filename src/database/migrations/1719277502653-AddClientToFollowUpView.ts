c13e94a05f56bec5d79b01fb1b8e87ccfcf7a90f
Sergio Pola <sergio.pola@keylogistics.cl>
2024-06-25 15:25:17 -0400

    Actualización migrate productivo

--- src/database/migrations/1719277502653-AddClientToFollowUpView.ts
+++ src/database/migrations/1719277502653-AddClientToFollowUpView.ts
@@ -5,11 +5,11 @@ export class AddClientToFollowUpView1719277502653 implements MigrationInterface 
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE VIEW \`client_to_do_follow_up_view\` AS SELECT \`c\`.\`id\`, \`c\`.\`name\`, \`c\`.\`rut\`, \`c\`.\`salary\`, \`c\`.\`savings\`, MAX(\`m\`.\`sentAt\`) AS \`date\` FROM \`clients\` \`c\` LEFT JOIN \`messages\` \`m\` ON  \`c\`.\`id\` = m.clientid AND \`m\`.\`deleted_at\` IS NULL WHERE \`c\`.\`deleted_at\` IS NULL GROUP BY \`c\`.\`id\`, \`c\`.\`name\`, \`c\`.\`rut\`, \`c\`.\`salary\`, \`c\`.\`savings\`, \`m\`.\`clientId\``);
-        await queryRunner.query(`INSERT INTO \`lidz_scoring\`.\`typeorm_metadata\`(\`database\`, \`schema\`, \`table\`, \`type\`, \`name\`, \`value\`) VALUES (DEFAULT, ?, DEFAULT, ?, ?, ?)`, ["lidz_scoring","VIEW","client_to_do_follow_up_view","SELECT `c`.`id`, `c`.`name`, `c`.`rut`, `c`.`salary`, `c`.`savings`, MAX(`m`.`sentAt`) AS `date` FROM `clients` `c` LEFT JOIN `messages` `m` ON  `c`.`id` = m.clientid AND `m`.`deleted_at` IS NULL WHERE `c`.`deleted_at` IS NULL GROUP BY `c`.`id`, `c`.`name`, `c`.`rut`, `c`.`salary`, `c`.`savings`, `m`.`clientId`"]);
+        await queryRunner.query(`INSERT INTO \`typeorm_metadata\`(\`database\`, \`schema\`, \`table\`, \`type\`, \`name\`, \`value\`) VALUES (DEFAULT, ?, DEFAULT, ?, ?, ?)`, ["lidz_scoring","VIEW","client_to_do_follow_up_view","SELECT `c`.`id`, `c`.`name`, `c`.`rut`, `c`.`salary`, `c`.`savings`, MAX(`m`.`sentAt`) AS `date` FROM `clients` `c` LEFT JOIN `messages` `m` ON  `c`.`id` = m.clientid AND `m`.`deleted_at` IS NULL WHERE `c`.`deleted_at` IS NULL GROUP BY `c`.`id`, `c`.`name`, `c`.`rut`, `c`.`salary`, `c`.`savings`, `m`.`clientId`"]);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DELETE FROM \`lidz_scoring\`.\`typeorm_metadata\` WHERE \`type\` = ? AND \`name\` = ? AND \`schema\` = ?`, ["VIEW","client_to_do_follow_up_view","lidz_scoring"]);
+        await queryRunner.query(`DELETE FROM \`typeorm_metadata\` WHERE \`type\` = ? AND \`name\` = ? AND \`schema\` = ?`, ["VIEW","client_to_do_follow_up_view","lidz_scoring"]);
         await queryRunner.query(`DROP VIEW \`client_to_do_follow_up_view\``);
     }
 
