e17345aa8e0683737f5567da535a354716b80596
Sergio Pola <sergio.pola@keylogistics.cl>
2024-06-25 20:38:33 -0400

    agregar tsdoc

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,7 +1,12 @@
+/**
+ * TypeOrm config used to run the migrations. Read the .env files o the .env of the current NODE_ENV enviroment.
+ */
+
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { DataSourceOptions, DataSource } from 'typeorm';
 
+// Load the env file
 const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
 const envPath = path.resolve(__dirname, `../../${envFile}`);
 
@@ -9,6 +14,7 @@ let d = dotenv.config({
   path: envPath,
 });
 
+// Extract the variables
 const {
   DATABASE_HOST,
   DATABASE_PORT,
@@ -17,6 +23,7 @@ const {
   DATABASE_NAME,
 } = d.parsed;
 
+// Configure datasource
 export const dataSourceOptionsRemote: DataSourceOptions = {
   type: 'mysql',
   host: DATABASE_HOST,
