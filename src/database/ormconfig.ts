339f13b85d7a03b2d747f01b4f8cca5a25eccc91
Sergio Pola <sergio.pola@keylogistics.cl>
2024-06-25 20:36:50 -0400

    Unificar configuración de base de datos para conexión y para migraciones

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -1,19 +1,36 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 import { DataSourceOptions, DataSource } from 'typeorm';
 
-export const dataSourceOptions: DataSourceOptions = {
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
+const envPath = path.resolve(__dirname, `../../${envFile}`);
+
+let d = dotenv.config({
+  path: envPath,
+});
+
+const {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASS,
+  DATABASE_NAME,
+} = d.parsed;
+
+export const dataSourceOptionsRemote: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'lidz_scoring',
+  host: DATABASE_HOST,
+  port: +DATABASE_PORT,
+  username: DATABASE_USER,
+  password: DATABASE_PASS,
+  database: DATABASE_NAME,
   entities: ['dist/**/*.entity.js'],
   synchronize: false,
-//   options: { trustServerCertificate: true },
+  //   options: { trustServerCertificate: true },
   logging: true,
   migrations: ['dist/database/migrations/*{.ts,.js}'],
 };
 
-const dataSource = new DataSource(dataSourceOptions);
+const dataSource = new DataSource(dataSourceOptionsRemote);
 
 export default dataSource;
